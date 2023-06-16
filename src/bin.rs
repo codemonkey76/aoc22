626d6c974ceacc07be4c4f5d26df185173ac180f
Shane Poppleton <shane@bjja.com.au>
2023-06-16 19:44:46 +1000

    Half-done adding timing

--- src/bin.rs
+++ src/bin.rs
@@ -1,6 +1,8 @@
 use aoc_lib::*;
 use clap::Parser;
+use std::time::{Instant, Duration};
 use std::fmt::{Display, Formatter, Result as FmtResult};
+use num_format::{Locale, ToFormattedString};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -20,38 +22,42 @@ fn main() {
 
     let day = args.day;
     let all = args.all;
+    let timer = Instant::now();
 
     if all {
         let mut results = Vec::with_capacity(25);
         println!("Running all days");
-        (1..=25).for_each(|day| results.push(run_day(day)));
+        (1..=25).for_each(|day| results.push(run_day(day, &timer)));
         results.iter().for_each(|result| println!("{}", result));
         return;
     }
 
     if let Some(day) = day {
         println!("Day: {:?}", day);
-        println!("{}", run_day(day));
+        println!("{}", run_day(day, &timer));
         return;
     }
 
     println!("No day specified");
 }
 
-fn run_day(day: u8) -> RunResult {
+fn run_day(day: u8, timer: &Instant) -> RunResult {
     println!("Running day: {}", day);
 
-    let run = match day {
-        1 => day01::run,
+    let run_both = match day {
+        1 => day01::run_both,
         _ => panic!("Day not implemented"),
     };
 
-    let answer_one = run(Part::One);
-    let answer_two = run(Part::Two);
+    let start = timer.elapsed();
+    let (answer_one, answer_two) = run_both();
+    let duration = timer.elapsed() - start;
+
     RunResult {
         day,
         answer_one,
         answer_two,
+        duration
     }
 }
 
@@ -59,14 +65,17 @@ struct RunResult {
     day: u8,
     answer_one: Output,
     answer_two: Output,
+    duration: Duration
 }
 
 impl Display for RunResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(
-            f,
-            "Day: {}\n\tPart One: {}\n\tPart Two: {}",
-            self.day, self.answer_one, self.answer_two
-        )
+        writeln!(f, "***************************************************")?;
+        writeln!(f, "*    Advent of Code: 2022, Day {:0>2}                 *", self.day)?;
+        writeln!(f, "*        Solution for...                          *")?;
+        writeln!(f, "*            Part One: {:_>6}                     *", self.answer_one.to_formatted_string(&Locale::en))?;
+        writeln!(f, "*            Part Two: {:_>6}                     *", self.answer_two)?;
+        writeln!(f, "*    Run Time: {:>10?}                             *", self.duration)?;
+        writeln!(f, "***************************************************")
     }
 }
